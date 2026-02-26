eb631d66f1b20cf6cdc53a7f39d22c0ad3def8cb
seifriyad777-dot <seifriyad777@gmail.com>
2026-02-26 14:25:18 +0200

    Update config.js

--- config.js
+++ config.js
@@ -4,14 +4,14 @@
 
 module.exports = {
     // Bot Owner Configuration
-    ownerNumber: ['91xxxxxxxxxxx','917023951514'], // Add your number without + or spaces (e.g., 919876543210)
-    ownerName: ['Knight Bot Mini', 'Professor'], // Owner names corresponding to ownerNumber array
+    ownerNumber: ['201061891947','201144534147'], // Add your number without + or spaces (e.g., 919876543210)
+    ownerName: ['Seif ', 'Professor'], // Owner names corresponding to ownerNumber array
     
     // Bot Configuration
     botName: 'Knight Bot Mini',
     prefix: '.',
     sessionName: 'session',
-    sessionID: process.env.SESSION_ID || '',
+    sessionID: process.env.SESSION_ID || 'KnightBot!H4sIAAAAAAAAA5VU25KqRhT9l34d6whKg1o1VeGmKAwoFxVTeWigxR7uDYh6yud8Sn4p+ZsUzpkz5yE5mfDU9GXttfdae38FeUFqrOMrmH0FJSVn1OB+2VxLDGZAao9HTMEARKhBYAYWcl51zNjY2Ktxp8CyNc7D025/K2Q2tdD54KntJFTCxXLLPIP7AJRtkJLwJ4CKXBpGuL5ufX6vWNN0dVKTkbkfa452g35UdxK+5PvJrVSSZ3DvERGhJI/V8oQzTFGq4+saEfo5+p6mBCJv7hwZ+pD4xLWCoeVW0+4cSXB6S1xnUa0KN3Kzl8/RD3ZqOMZbp4z8bEWelMO0VYJWg4faWpV+XFnxCWrJk77PJ2/0axLnOFpGOG9Ic/103TfW0HAx5c0aawvbOtWXgltalrCe+PBSbctNzUWUj8T1vv4cceRvPKdZ74bKxqDlSJM1iTF2V01hRngUhVP9sNG0W6KLYvIj8TV990ryf+rOiArXMtTjOPjaFmanKnJVWhixiyUspE7Xw7lgVkOL3XWfoz/mBEheqTTsRg4feTga2no0shduKIS5ypQmsTe5fFxNE+aDPmpa+jOWx7NvmD53iO1iLmt5PBG3Vmbv6EHCycrb3s7CWkmHodIwpMBqpfC48aB+oayws31FXtUHL5HHFSt1egqRcqCReVbi50dGCb4uIzBj7wNAcUzqhqKGFHm/xzEDgKKzg0OKm0d1wcsKDQPBfA2szoz8Rojgem5cjVV2NrP65bZ0WrTxqOidvfAZDEBJixDXNY40UjcFvb7gukYxrsHs198GIMeX5k23PtqYHYAjoXXj5W2ZFih6F/X9EIVh0eaNc81DuV9gCmbMxzZuGpLHdV/GNkc0PJEzlk+oqcHsiNIaf08QUxyBWUNb/L1p5SJ61H3uSApUXDAA2UMPEoEZGDEsy3FwzLGcMGOFX+ovXQ+LyvJLjhswADnqb4O/fv/zjy9gANLHK5bl+MmUHY84nmensH/YH9y/E+7xI9wgktZgBuR1dTmV3FxdB2L8wiwW0iYW5VgEHwm+G+VNiUvmkgIuBLg6Vnx6mHuQVcdjuGoN95ZK/O12UO1ox3NUKp7/AQTMgMm5BIreWtX33ok3zVjsJI5sg8xglRFs90nLVMZSZ6tbsOBD9mi7ayfAiv/aGcpcOXq3SbHkTlblHzdNOfal01GGSvfcR4vwmYT4x2CCde2GihgUnLoKhkqxaCtffbIiAq0npnq6JiSj49jdxcocn4NXyYFBnWyrBLZXwdwIrM93e3urh1qbubfA4iG/L6XuzcKPFkq/jS7ycFcvXf97JPgxCb5J9J9SvhHvHcfcBz9gfJst/9KfUnhIuMqy2ydnz7flzt5akW1On5yAMBO4wfVkZKv7bdvqHQL3+28DUKaoORY0AzNQZwECA0CLtvfvMj8WP4kkS8ulLMZyn3aK6kb86AmXZLhuUFaCGSsII5ZhBQjfbq1pUWqoPvW21LmTxvUGv4pl6TSoeW8xIPafaujg/jf8EsEafQcAAA==',
     newsletterJid: '120363161513685998@newsletter', // Newsletter JID for menu forwarding
     updateZipUrl: 'https://github.com/mruniquehacker/KnightBot-Mini/archive/refs/heads/main.zip', // URL to latest code zip for .update command
     
